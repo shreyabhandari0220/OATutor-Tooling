5f2c050c13d3bc9f59d1de4051912369cd1bd634
Rishi B <rishibalakrishnan@berkeley.edu>
2021-01-02 16:48:27 -0800

    Revert "added scripts"
    This reverts commit 4479e8974a335183fb5e71ccf0c5fd6bcdb5dd90.

--- Open/LinEqua2/LinEqua2.js
+++
@@ -1,1 +0,0 @@
-import React from 'react'; import { InlineMath } from 'react-katex';import steps from "./LinEqua2-index.js"; const problem = {id: "LinEqua2", title: "Solving an Equation in One Variable", body: "Solve the following equation:", steps: steps, }; export { problem };
--- Open/real1/steps/real1c/real1c.js
+++
@@ -1,1 +0,0 @@
-import hints from "./real1c-index.js"; const step = {id: "real1c", stepAnswer: ["-0.71"], problemType: "MultipleChoice", stepTitle: "What is -5/7? (To the nearest hundredth)", stepBody: "", choices: ["-0.25", "-0.71", "0.57", "-0.63"], answerType: "string", hints: hints}; export {step};
