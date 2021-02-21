234ea841d4883dcd4c700a3b58851d8cff77e899
TinnaLiu <tinnaliu@berkeley.edu>
2021-02-21 19:19:17 +0800

    solved inf and minus not recognized error

--- TerminalScript/process_text.py
+++ TerminalScript/process_text.py
@@ -5,16 +5,19 @@ from pytexit import py2tex
 
 supported_operators = ["**", "/", "*", "+", ">", "<", "="]
 supported_word_operators = ["sqrt", "abs", "inf"]
+answer_only_operators = ["-"]
 replace = {"⋅" : "*", "−" : "-", "^" : "**", "𝑥" : "x", "𝑎" : "a", "𝑏" : "b", "𝑦" : "y", "–": "-", "≥" : ">=", "≤": "<=", "∪" : "U"}
 conditionally_replace = {"[" : "(", "]" : ")"}
 regex = re.compile("|".join(map(re.escape, replace.keys())))
 
 #Figure out way to deal with equal signs
-def preprocess_text_to_latex(text, tutoring=False, stepMC= False):
+def preprocess_text_to_latex(text, tutoring=False, stepMC=False, stepAns=False):
     text = str(text)
     text = regex.sub(lambda match: replace[match.group(0)], text)
-    if not re.findall("[\[|\(][-\d\s\w]+,[-\d\s\w]+[\)|\]]", text): #Checking to see if there are coordinates/intervals before replacing () with []
+    coordinate = True
+    if not re.findall("[\[|\(][-\d\s\w/]+,[-\d\s\w/]+[\)|\]]", text): #Checking to see if there are coordinates/intervals before replacing () with []
         text = regex.sub(lambda match: conditionally_replace[match.group(0)], text)
+        coordinate = False
     
     
     #Account for space in sqrt(x, y)
@@ -24,22 +27,24 @@ def preprocess_text_to_latex(text, tutoring=False, stepMC= False):
     text = re.sub("\([\s]*([-\d]+)[\s]*,[\s]*([-\d]+)[\s]*\)", "(\g<1>,\g<2>)", text) #To account for coordinates
     for operator in supported_operators:
         text = re.sub("(\s?){0}(\s?)".format(re.escape(operator)), "{0}".format(operator), text)
-        
+
     words = text.split()
     latex = False
     for i in list(range(len(words))):
         word = words[i]
-        
-        if any([op in word for op in supported_operators]) or any([op in word for op in supported_word_operators]):
+        if ((stepMC or stepAns) and any([op in word for op in answer_only_operators])) or \
+            any([op in word for op in supported_operators]) or \
+            any([op in word for op in supported_word_operators]):
             punctuation = re.findall("[\?\.,:]", word) #Capture all the punctuation at the end of the sentence
             if punctuation:
                 punctuation = punctuation[0]
             else:
                 punctuation = ""
-            word = re.sub("[\?\.,:]", "", word)
+            if not coordinate:
+                word = re.sub("[\?\.,:]", "", word)
             try:                
                 sides = re.split('(=|U|<=|>=)', word)
-                sides = [handle_word(side) for side in sides]
+                sides = [handle_word(side, coordinate) for side in sides]
                 new_word = ""
                 if tutoring and stepMC:
                     new_word = "$$" + "".join(sides) + "$$"
@@ -64,12 +69,11 @@ def preprocess_text_to_latex(text, tutoring=False, stepMC= False):
     text = " ".join(words)
     return text, latex
 
-def handle_word(word):
+def handle_word(word, coordinates):
     latex_dic = {"=": "=", "U": " \cup ", "<=" : " \leq ", ">=" : " \geq "}
     if word in latex_dic:
         return latex_dic[word]
     
-    coordinates = re.findall("[\(|\[][-\d\s]+,[-\d\s]+[\)|\]]",word)
     if coordinates:
         word = re.sub("inf", r"\\infty", word)
         return word
@@ -92,7 +96,15 @@ def handle_word(word):
     word = re.sub(r"sqrt\*", r"sqrt", word)
     word = re.sub(r"abs\*", r"abs", word)
     word = re.sub(r"pm\*", r"pm", word)
-    word = py2tex(word, simplify_output=False)
+    
+    # handle double negative sign. Looks like py2tex handles double negative signs wrong.
+    double_neg = re.search('-\(\-', word)
+    if double_neg:
+        before_neg = word[:double_neg.start()]
+        after_neg = word[double_neg.end() - 3:]
+        word = py2tex(before_neg, simplify_output=False)[:-2] + py2tex(after_neg, simplify_output=False)[2:]
+    else:
+        word = py2tex(word, simplify_output=False)
     
     #Here do the substitutions for the things that py2tex can't handle
     for item in scientific_notation:
