6159995bf7381a0a4f9790d130d63eaad2425622
TinnaLiu <tinnaliu@berkeley.edu>
2021-03-11 12:21:38 +0800

    refreshed content 3-11

--- OpenStax Content/DivMul23/DivMul23.js
+++ OpenStax Content/DivMul23/DivMul23.js
@@ -1,1 +1,1 @@
-import React from 'react'; import { InlineMath } from 'react-katex';import steps from "./DivMul23-index.js"; const problem = {id: "DivMul23", title: "Translate and Solve", body: "", steps: steps, }; export { problem };
+import React from 'react'; import { InlineMath } from 'react-katex';import steps from "./DivMul23-index.js.js"; const problem = {id: "DivMul23", title: "Translate and Solve", body: "", steps: steps, }; export { problem };
