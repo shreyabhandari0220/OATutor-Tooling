baf6aed2c8ccc0bf1615c2f7d1d694abd0157b4e
matthew29tang <matthew29tang@gmail.com>
2021-04-29 01:03:46 -0700

    Take in sheetName

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-echo "[STAGE 1]: Installing python dependencies (This can take 1-2 minutes)"
+set -e
+echo "\n[STAGE 1]: Installing python dependencies (This can take 1-2 minutes)"
 pip install -q -r ./TerminalScript/requirements.txt
 cd TerminalScript
 
-echo "[STAGE 2] Running Script. Script warnings (if any) will print below"
-python ./process_sheet.py online "1Lp0uGtQsuzxzrm1TSctuZttJRrvaG0E5cwT-75UKZeY" "5.6 Rational Functions" TRUE > /dev/null
+echo "Note: Currently only supports College Algebra sheet."
+read -p 'Enter sheet name: ' sheetName
 
+echo "\n[STAGE 2] Running Script. Script warnings (if any) will print below"
+python ./process_sheet.py online "1Lp0uGtQsuzxzrm1TSctuZttJRrvaG0E5cwT-75UKZeY" "$sheetName" TRUE FALSE 2>&1
 
-echo "[STAGE 2]: Creating index files..."
+echo "\n[STAGE 3]: Creating index files..."
 cd ..
 node ./util/indexGenerator.js
 
-echo "[STAGE 3]: Running validator. Validator warnings (if any) will print below."
+echo "\n[STAGE 4]: Running validator. Validator warnings (if any) will print below."
 node --experimental-modules .\postScriptValidator.js 
 
-echo "[STAGE 4]: Cleaning up directory..."
+echo "\n[STAGE 5]: Cleaning up directory..."
 rm -rf ./OpenStax1
