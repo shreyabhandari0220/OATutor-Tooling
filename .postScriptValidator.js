b2e9d464cbcff1b6bad3ed7a5d6b01c961602b85
matthew29tang <matthew29tang@gmail.com>
2021-04-28 23:44:09 -0700

    Add validator to replit

---
+++ .postScriptValidator.js
@@ -0,0 +1,79 @@
+import problems from './OpenStax1/problemPool.js';
+
+var stepCounter = 0;
+var hintCounter = 0;
+var errors = 0;
+var errorList = {};
+
+// node --experimental-modules .\postScriptValidator.js
+
+problems.map(problem => {
+  problem.steps.map(step => {
+    stepCounter += 1;
+    // Check MC answer is one of choices
+    if (step.problemType === "MultipleChoice") {
+      if (typeof step.choices === 'undefined') {
+        console.log("[ERROR] [" + step.id + "] Has no mc options");
+        console.log(step);
+        console.log("=================");
+        errors += 1;
+        errorList[step.id] = "Mc question contains no options";
+      }
+      else if (!step.choices.some(choice => choice === step.stepAnswer[0])) {
+        console.log("[ERROR] [" + step.id + "] No mc options match answer verbatim");
+        console.log(step);
+        console.log("=================");
+        errors += 1;
+        errorList[step.id] = "No option matches answer verbatim";
+      }
+    }
+    else if (step.problemType === "TextBox") {
+      // Check TextBox answers with commas are string type
+      if (step.answerType === "arithmetic" && step.stepAnswer[0].search(",") != -1) {
+        console.log("[ERROR] [" + step.id + "] Answer type must be string if contains comma.");
+        console.log(step);
+        console.log("=================");
+        errors += 1;
+        errorList[step.id] = "Arithmetic answer contains comma";
+      }
+    }
+
+    step.hints['defaultPathway'].map(hint => {
+      hintCounter += 1;
+      if (hint.problemType === "MultipleChoice") {
+        if (typeof hint.choices === 'undefined') {
+          console.log("[ERROR] [" + hint.id + "] Has no mc options");
+          console.log(hint);
+          console.log("=================");
+          errors += 1;
+          errorList[hint.id] = "Mc question contains no options";
+        }
+        else if (!hint.choices.some(choice => choice === hint.hintAnswer[0])) {
+          console.log("[ERROR] [" + hint.id + "] No mc options match answer verbatim");
+          console.log(hint);
+          console.log("=================");
+          errors += 1;
+          errorList[hint.id] = "No option matches answer verbatim";
+        }
+      }
+
+      else if (hint.problemType === "TextBox") {
+        // Check TextBox answers with commas are string type
+        if (hint.answerType === "arithmetic" && hint.hintAnswer[0].search(",") != -1) {
+          console.log("[ERROR] [" + hint.id + "] Answer type must be string if contains comma.");
+          console.log(hint);
+          console.log("=================");
+          errors += 1;
+          errorList[hint.id] = "Arithmetic answer contains comma";
+        }
+      }
+    })
+  })
+});
+console.log("Validated " + problems.length + " problems, containing " + stepCounter + " steps, and " + hintCounter + " hints.");
+console.log("Found " + errors + " errors.");
+for (var k in errorList) {
+  console.log(k + ": " + errorList[k])
+}
+
+//console.log(errorList);
--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-echo "Installing python dependencies... This can take 1-2 minutes."
+echo "[STAGE 1]: Installing python dependencies (This can take 1-2 minutes)"
 pip install -q -r ./TerminalScript/requirements.txt
 cd TerminalScript
-echo "Running Script... Script warnings (if any) will print below"
-python ./process_sheet.py online "1Lp0uGtQsuzxzrm1TSctuZttJRrvaG0E5cwT-75UKZeY" "1.1 Real Numbers" TRUE > /dev/null
+
+echo "[STAGE 2] Running Script. Script warnings (if any) will print below"
+python ./process_sheet.py online "1Lp0uGtQsuzxzrm1TSctuZttJRrvaG0E5cwT-75UKZeY" "5.6 Rational Functions" TRUE > /dev/null
 
 
-echo "Installing js dependencies... This can take 1-2 minutes."
+echo "[STAGE 2]: Creating index files..."
 cd ..
-
-echo "Creating index files..."
 node ./util/indexGenerator.js
+
+echo "[STAGE 3]: Running validator. Validator warnings (if any) will print below."
+node --experimental-modules .\postScriptValidator.js 
