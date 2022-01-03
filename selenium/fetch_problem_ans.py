315855c55d479568cbd0fec866a7e66b2a826404
Tinna Liu <tinnaliu@berkeley.edu>
2022-01-03 14:59:16 -0800

    fixed xpath, added hint

--- selenium/fetch_problem_ans.py
+++ selenium/fetch_problem_ans.py
@@ -2,6 +2,9 @@ import os
 import sys
 from os.path import dirname
 import re
+import ast
+
+from problem import *
 
 CURRENT_PATH = os.getcwd()
 CONTENT_PATH = os.path.join(dirname(CURRENT_PATH), 'OpenStax Content')
@@ -12,6 +15,24 @@ def get_all_content_filename(content_path=CONTENT_PATH):
     Ex. ['quadratic17', 'IneqApp2', 'line14', 'partfrac26', 'uni27',...]
     """
     return [direc for direc in os.listdir(content_path) if direc != "stepfiles.txt" and direc != ".DS_Store"]
+
+def process_hint_answer(hint_text):
+    hint_list = hint_text[:].split("}, {")
+    hint_answers = []
+    for hint in hint_list:
+        if not re.search(", type: \"(\w+)\"", hint):
+            break
+        hint_type = re.search(", type: \"(\w+)\"", hint).group(1)
+        if hint_type == "hint":
+            hint_answers.append(hint_type)
+        elif hint_type == "scaffold":
+            scaf_type = re.search('problemType:\s*\"(.*?)\",\s*answerType:', hint).group(1)
+            scaf_ans = re.search('hintAnswer:\s*\[\"(.*?)\"\],\s*dependencies:', hint).group(1)
+            scaf_ans = scaf_ans.replace('\\\\', '\\')
+            if scaf_type == "TextBox":
+                scaf_type += " " + re.search('answerType:\s*\"(.*?)\",\s*hintAnswer:', hint).group(1)
+            hint_answers.append([scaf_ans, scaf_type])
+    return hint_answers
 
 def fetch_problem_ans_info(problem_name, verbose=False):
     """
@@ -34,8 +55,11 @@ def fetch_problem_ans_info(problem_name, verbose=False):
     all_steps_dir = os.path.join(CONTENT_PATH, problem_name, "steps")
     step_name_list = sorted([x for x in next(os.walk(all_steps_dir))][1]) # gives a list of subdirectory names
     problem_info = []
+    step_obj_list = []
 
     for step_name in step_name_list:
+
+        # fetch step type and answer
         step_path = os.path.join(all_steps_dir, step_name, step_name + '.js')
         with open(step_path) as step_file:
             data = step_file.read()
@@ -49,13 +73,30 @@ def fetch_problem_ans_info(problem_name, verbose=False):
                 variabilization = re.search('variabilization: {([^}]+)}', data).group(1)
                 var_dict = dict(re.findall('([^,:\s]+)+:\s(\[[^]]+\])', variabilization))
                 for k, v in var_dict.items():
-                    var_dict[k] = re.search("\[\"(\w+)\",", v).group(1)
-                step_ans = re.sub("@{(\w+)}", lambda m: var_dict.get(m.group(1), m.group(1)), step_ans)
-            step_ans = step_ans.replace('\\\\', '\\')
+                    var_dict[k] = ast.literal_eval(v)
+                ans_lst = []
+                for i in range(len(list(var_dict.values())[0])):
+                    ans_lst.append(re.sub("@{(\w+)}", lambda m: var_dict.get(m.group(1))[i], step_ans).replace('\\\\', '\\'))
+                step_ans = ans_lst # list of all possible answers
+            else:
+                step_ans = step_ans.replace('\\\\', '\\')
             problem_info.append([step_ans, step_type])
-    if verbose:
-        print(book_name, problem_info)
-    return [book_name, problem_info]
+
+        # fetch hint and scaffold answer
+        hint_path = os.path.join(all_steps_dir, step_name, "tutoring", step_name + "DefaultPathway.js")
+        with open(hint_path) as hint_file:
+            hint_text = hint_file.read()
+            hint_info_list = process_hint_answer(hint_text)
+
+        step = Step(step_name, step_ans, step_type, hint_info_list)
+        step_obj_list.append(step)
+    
+    problem = Problem(book_name, problem_name, step_obj_list)
+
+    # if verbose:
+    #     print(book_name, problem_info)
+
+    return problem
 
 
 if __name__ == '__main__':
