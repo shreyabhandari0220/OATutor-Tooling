77113f6969df03a3a8198ebce40ea568e6cc9877
Tinna Liu <tinnaliu@berkeley.edu>
2022-01-07 23:30:45 -0800

    switched to css selector

--- selenium/generate_script.py
+++ selenium/generate_script.py
@@ -6,7 +6,7 @@ def generate_script_arithmetic(selector, answer):
     answer = re.sub('\$', '', answer)
     selector = re.sub(r"\"", "\\\"", selector)
     script = "document = new Document();\n"
-    script += "ans = document.evaluate(\"{}\", document, null, 9, null).singleNodeValue;\n".format(selector)
+    script += "ans = document.querySelector(\"{}\");\n".format(selector)
     script += "var MQ = MathQuill.getInterface(2);\n"
     script += "mathField = MQ.MathField(ans);\n"
     script += "mathField.focus();\n"
